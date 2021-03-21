84e3c41823670d67e1a69e941dff07fbd7856403
kurita373 <minami0220252525@gmail.com>
2021-03-21 17:02:34 +0900

    自己修正１

--- q6/work/js/app.js
+++ q6/work/js/app.js
@@ -1,15 +1,22 @@
 $(function () {
   $(".select-box").change(function () {
+    //セレクトボックス変更した時
     var r = $("option:selected").val();
+    //選択した値を代入
     var count = $(".food-list").children().length;
-    console.log(count);
+    //フードリストの要素数を取得
     for (var i=0; i<count; i++){
-      var food =$(".food-list li").eq(i).data("categoryType");
+      //フードリストの要素数分、forで繰り返す
       var kind =$(".food-list li").eq(i)
+      var food =$(".food-list li").eq(i).data("categoryType");
+      //フードリストのデータ取得
       if(food === r){
+        //選択したvalueと同じdataだった場合
         kind.show();
       }else{
+        //選択したvalueと違うdataだった場合
         if(r === "all"){
+          //選択したvalueがallだった場合
           $(".food-list li").show();
         }else{
           kind.hide();
