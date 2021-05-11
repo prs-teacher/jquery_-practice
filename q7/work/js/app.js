0d7f8aa5f5abd4ae203fe665574f62a3f96e9849
kurita373 <minami0220252525@gmail.com>
2021-05-11 21:17:51 +0900

    修正１

--- q7/work/js/app.js
+++ q7/work/js/app.js
@@ -1,59 +1,48 @@
 $(function () {
   $(".btn__submit").on("click", function () {
   //ボタンクリックしたら発動
-  var label__text = $(".label__text").eq(0).text();
-  //指定したindex番号のテキスト代入
-  console.log(label__text);
+  console.log($(".label__text").eq(0).text());
   //指定したindex番号のテキスト表示
-  var family__name = $("#family__name").val();
+  const familyName = $("#family__name").val();
   //family__nameの値代入
-  console.log(family__name);
+  console.log(familyName);
   //family__nameの値表示
-  var label__text = $(".label__text").eq(1).text();
-  console.log(label__text);
-  var given__name = $("#given__name").val();
-  console.log(given__name);
-  var label__text = $(".label__text").eq(2).text();
-  console.log(label__text);
-  var year = $(".year option:selected").val();
-  var month = $(".month option:selected").val();
-  var day = $(".day option:selected").val();
+  //var labelText = $(".label__text").eq(1).text();
+  console.log($(".label__text").eq(1).text());
+  const givenName = $("#given__name").val();
+  console.log(givenName);
+  console.log($(".label__text").eq(2).text());
+  const year = $(".year option:selected").val();
+  const month = $(".month option:selected").val();
+  const day = $(".day option:selected").val();
   console.log(year+"年"+month+"月"+day+"日");
-  var label__text = $(".label__text").eq(3).text();
-  console.log(label__text);
-  var gender = $('[name="gender"]:checked').val();
+  console.log($(".label__text").eq(3).text());
+  const gender = $('[name="gender"]:checked').val();
   console.log(gender);
-  var label__text = $(".label__text").eq(4).text();
-  console.log(label__text);
-  var occupation = $(".occupation option:selected").val();
+  console.log($(".label__text").eq(4).text());
+  const occupation = $(".occupation option:selected").val();
   console.log(occupation);
-  var label__text = $(".label__text").eq(5).text();
-  console.log(label__text);
-  var account__name = $("#account__name").val();
-  console.log(account__name);
-  var label__text = $(".label__text").eq(6).text();
-  console.log(label__text);
-  var email = $("#email").val();
+  console.log($(".label__text").eq(5).text());
+  const accountName = $("#account__name").val();
+  console.log(accountName);
+  console.log($(".label__text").eq(6).text());
+  const email = $("#email").val();
   console.log(email);
-  var label__text = $(".label__text").eq(7).text();
-  console.log(label__text);
-  var password = $("#password").val();
+  console.log($(".label__text").eq(7).text());
+  const password = $("#password").val();
   console.log(password);
-  var label__text = $(".label__text").eq(8).text();
-  console.log(label__text);
-  var duplication__password = $("#duplication__password").val();
-  console.log(duplication__password);
-  var label__text = $(".label__text").eq(9).text();
-  console.log(label__text);
-  var address = $("#address").val();
+  console.log($(".label__text").eq(8).text());
+  const duplicationPassword = $("#duplication__password").val();
+  console.log(duplicationPassword);
+  console.log($(".label__text").eq(9).text());
+  const address = $("#address").val();
   console.log(address);
-  var label__text = $(".label__text").eq(10).text();
-  console.log(label__text);
-  var tel = $("#tel").val();
+  console.log($(".label__text").eq(10).text());
+  const tel = $("#tel").val();
   console.log(tel);
-  var content__title = $(".content__title").eq(3).text();
-  console.log(content__title);
-  var subscription = $("input[name=subscription]:checked").map(function(){
+  const contentTitle = $(".content__title").eq(3).text();
+  console.log(contentTitle);
+  const subscription = $("input[name=subscription]:checked").map(function(){
     //チェックされている値を配列に格納
     return $(this).val();
     //値を返す
--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -9,65 +9,92 @@
 // }).fail(function (err) {
 //   displayError(err)
 // });
-const searchWord = $("#search-input").val();
-const pageCount = 0;
 
 $(function () {
+  var pageCount = 1;
   $(".search-btn").on("click",function (){
+    //サーチボタンクリックしたら
+    $(".message").remove();
+    //★クラスメッセージ削除
     const searchWord =  $("#search-input").val();
-    console.log(searchWord);
-    const pageCount = 0;
-
+    //★検索ワードの代入
+    //console.log(searchWord);
     const settings = {
       "url": `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
-      //リクエスト送信先
+      //★リクエスト送信先
       "method": "GET",
-      //リクエストタイプ
+      //★リクエストタイプ
     }
 
-    $.ajax(settings).done(function (response,textStatus, jqXHR) {
-      //成功の場合の処理
+    function test(response){
+      //console.log(response)
       const result = response['@graph'];
-      //@graphの取り出し
-      console.log(response);
-      console.log(result);
-      console.log(result[0]['@id']);
+      //★@graphの取り出し
+      //console.log(response);
+      //console.log(result);
+      //console.log(result[0]['@id']);
       const items = result[0]['items'];
-      console.log(items);
-      for(var i = 0; i < 20; i++){
-        //ループ処理
-        console.log(items[i]['title']);
-        $listinner = $('<div class="list-inner"></div>');
-        //＄listinnerに代入
-        $listinner.append('<p>タイトル：'+items[i]['title']+'</p>');
-        $listinner.append('<p>作者：'+items[i]['dc:creator']+'</p>');
-        $listinner.append('<p>出版社：'+items[i]['dc:publisher']+'</p>');
-        $listinner.append('<a href="'+items[i]['@id']+'"target="_blank">書籍情報</a>');
-        console.log(items[i]['@id']);
-        //listinnerの子要素としてDOMの挿入
-        const $div = $('<li class="lists-item" ></li>');
-        //$divに代入
-        $div.append($listinner);
-        //＄divの子要素としてDOMの挿入
-        $div.appendTo('.lists');
-        //.listsの子要素にDOM挿入
+      //console.log(items);
+      const totalResults = result[0]['opensearch:totalResults'];
+      //console.log(totalResults);
+      if(totalResults > 0){
+        for(var i = 0; i < 100; i++){
+          //★ループ処理
+          //console.log(items[i]['title']);
+          $listinner = $('<div class="list-inner"></div>');
+          //＄★listinnerに代入
+          $listinner.append('<p>タイトル：'+items[i]['title']+'</p>');
+          $listinner.append('<p>作者：'+items[i]['dc:creator']+'</p>');
+          $listinner.append('<p>出版社：'+items[i]['dc:publisher']+'</p>');
+          $listinner.append('<a href="'+items[i]['@id']+'"target="_blank">書籍情報</a>');
+          //console.log(items[i]['@id']);
+          //★listinnerの子要素としてDOMの挿入
+          const $div = $('<li class="lists-item" ></li>');
+          //★$divに代入
+          $div.append($listinner);
+          //★＄divの子要素としてDOMの挿入
+          $div.appendTo('.lists');
+      }}
+      if(totalResults　== 0){
+        $(".inner").prepend('<div class="message">検索結果が見つかりませんでした。<br>別のキーワードで検索してください。</div>');
       }
+    };
+    function vary(){
+      $("#search-input").change(function(){
+      　//★検索ワード違うとき
+        //var val = $(this).val();
+        //console.log(val);
+        $(".lists").empty();
+        ////★DOMの削除
+        pageCount = 1;
+        //★１ページめに戻る
+      });
+    }
+
+    $.ajax(settings).done(function (response) {
+      //★成功の場合の処理
+      pageCount = pageCount+1;
+      //★ページを増やす
+      vary();
+      test(response);
       }).fail(function (jqXHR, textStatus, errorThrown) {
-        //エラーの場合の処理
+        //★エラーの場合の処理
         $(".inner").prepend('<div class="message">正常に通信できませんでした。<br>インターネットの接続の確認をしてください。</div>');
-        //DOMの挿入
-        console.log("ajax通信に失敗しました");
-        console.log("jqXHR          : " + jqXHR.status); // HTTPステータスが取得
-        console.log("textStatus     : " + textStatus);    // タイムアウト、パースエラー
-        console.log("errorThrown    : " + errorThrown.message); // 例外情報
-        console.log("URL            : " + url);
+        //★DOMの挿入
+        //console.log("ajax通信に失敗しました");
+        //console.log("jqXHR          : " + jqXHR.status); // HTTPステータスが取得
+        //console.log("textStatus     : " + textStatus);    // タイムアウト、パースエラー
+        //console.log("errorThrown    : " + errorThrown.message); // 例外情報
       });
   });
+
   $(".reset-btn").on("click",function(){
-    //リセットボタンイベント
+    //★リセットボタンイベント
+    $(".message").remove();
+    //★DOMの削除
     $("#search-input").val("");
-    //リセットボタンイベント空
+    //★リセットボタンイベント空
     $(".lists").empty();
-    //DOMの削除
+    //★DOMの削除
   });
 });
